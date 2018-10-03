1433065100a22960536eeef738068610500438e4
graemeerickson <graemeerickson@gmail.com>
2018-10-03 14:30:27 -0700

    Changed repo to Repository to use Pascal Case. Used DataContract to map it to the 'repo' nodes from the JSON response.

---
+++ Repository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace WebAPIClient
+{
+    [DataContract(Name="repo")]
+    public class Repository
+    {
+        public string name;
+    }
+}
--- repo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace WebAPIClient
-{
-    public class repo
-    {
-        public string name;
-    }
-}
