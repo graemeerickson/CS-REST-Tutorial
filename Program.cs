5b334305cb510a661ba23ff40c10b2bdd61c7569
graemeerickson <graemeerickson@gmail.com>
2018-10-03 14:34:01 -0700

    Updated 'repo' class references to 'Repository'

--- Program.cs
+++ Program.cs
@@ -23,9 +23,9 @@ namespace WebAPIClient
                 new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
             client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
 
-            var serializer = new DataContractJsonSerializer(typeof(List<repo>));
+            var serializer = new DataContractJsonSerializer(typeof(List<Repository>));
             var streamTask = client.GetStreamAsync("https://api.github.com/orgs/dotnet/repos");
-            var repositories = serializer.ReadObject(await streamTask) as List<repo>;
+            var repositories = serializer.ReadObject(await streamTask) as List<Repository>;
 
             foreach (var repo in repositories)
                 Console.WriteLine(repo.name);
