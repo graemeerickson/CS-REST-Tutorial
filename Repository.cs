325f821cd5d879502b8ad240cb9e357ea6abdf2b
graemeerickson <graemeerickson@gmail.com>
2018-10-03 17:06:23 -0700

    Added properties to the Repository class definition to accept additional data fields from the JSON response

--- Repository.cs
+++ Repository.cs
@@ -7,6 +7,18 @@ namespace WebAPIClient
     public class Repository
     {
         [DataMember(Name = "name")]
-        public string Name { get; set; };
+        public string Name { get; set; }
+
+        [DataMember(Name = "description")]
+        public string Description { get; set; }
+
+        [DataMember(Name = "html_url")]
+        public Uri GitHubHomeUrl { get; set; }
+
+        [DataMember(Name = "homepage")]
+        public Uri Homepage { get; set; }
+
+        [DataMember(Name = "watchers")]
+        public int Watchers { get; set; }
     }
 }
